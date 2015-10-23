04ca0f8d7af7cb1e91b7f1ee0f031e3ec2d5f296
Denis Simon Soneira <denis.simon@gmail.com>
2015-10-23 15:00:56 +0200

    ejercicio 5

---
+++ src/main/java/org/dojo/java8/ejercicio5/LambdaExpert.java
@@ -0,0 +1,110 @@
+package org.dojo.java8.ejercicio5;
+
+import org.dojo.java8.model.Child;
+import org.dojo.java8.model.Person;
+import org.dojo.java8.model.PersonLibrary;
+
+import java.util.*;
+
+public class LambdaExpert {
+
+	private List<Person> persons = PersonLibrary.getPersons("/person_data_with_children.json");
+
+	// TODO: use map, flatMap, sorted and limit
+	public List<Child> firstTwoYoungerChildsByName() {
+		List<Child> allChildren = new ArrayList<>();
+
+		for (Person person : persons) {
+			allChildren.addAll(person.getChildren());
+		}
+
+		Collections.sort(allChildren, new ChildComparator());
+
+		return allChildren.subList(0, 2);
+	}
+
+	private class ChildComparator implements Comparator<Child> {
+		@Override
+		public int compare(Child c1, Child c2) {
+			int nameComparaison = c1.getName().compareTo(c2.getName());
+			if (nameComparaison == 0) {
+				return Integer.compare(c1.getAge(), c2.getAge());
+			} else {
+				return nameComparaison;
+			}
+		}
+	}
+
+	// TODO: NOTE! DO NOT USE STREAMS
+	// TODO: refactor Map computation with Map merge method and you can
+	// eventually change loop by Iterator forEach method
+	// TODO: use static reference to the Integer sum method
+	public Map<String, Long> countWord(List<String> words) {
+
+		Map<String, Long> count = new HashMap<>();
+
+		for (String word : words) {
+
+			Long countOfCurrentWord = count.get(word);
+			if (countOfCurrentWord == null) {
+				countOfCurrentWord = 0L;
+			}
+
+			count.put(word, countOfCurrentWord + 1L);
+		}
+
+		return count;
+	}
+
+	// TODO: NOTE! USE STREAMS
+	// TODO use Collectors.groupingBy with Collectors.counting
+	public Map<String, Long> countWordStream(List<String> words) {
+
+		Map<String, Long> count = new HashMap<>();
+
+		for (String word : words) {
+
+			Long countOfCurrentWord = count.get(word);
+			if (countOfCurrentWord == null) {
+				countOfCurrentWord = 0L;
+			}
+
+			count.put(word, countOfCurrentWord + 1L);
+		}
+
+		return count;
+	}
+
+	// TODO use Collectors.collectingAndThen, Collectors.groupingBy with
+	// Collectors.counting
+	// TODO to complete the refactor use max map and orElse
+	public int retrieveMostCommonAge() {
+		Map<Integer, Integer> numberPeopleByAge = new HashMap<>();
+		Integer personsByAge;
+		for (Person person : persons) {
+
+			personsByAge = numberPeopleByAge.get(person.getAge());
+			if (personsByAge == null) {
+				personsByAge = 0;
+				numberPeopleByAge.put(person.getAge(), personsByAge);
+			}
+			personsByAge++;
+
+			numberPeopleByAge.put(person.getAge(), personsByAge);
+
+		}
+
+		Map.Entry<Integer, Integer> maxEntry = null;
+
+		for (Map.Entry<Integer, Integer> entry : numberPeopleByAge.entrySet()) {
+			if (maxEntry == null || entry.getValue() > maxEntry.getValue()) {
+				maxEntry = entry;
+			}
+
+		}
+
+		return maxEntry.getKey();
+
+	}
+
+}
---
+++ src/test/java/org/dojo/java8/ejercicio5/LambdaExpertTest.java
@@ -0,0 +1,63 @@
+package org.dojo.java8.ejercicio5;
+
+import org.assertj.core.data.MapEntry;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LambdaExpertTest {
+
+    LambdaExpert lambdaExpert = new LambdaExpert();
+
+    @Test
+    public void should_count_words() {
+        List<String> words = new ArrayList<>();
+        words.add("java");
+        words.add("javascript");
+        words.add("java");
+        words.add("scala");
+        words.add("java");
+        words.add("scala");
+
+        Map<String, Long> wordCount = lambdaExpert.countWord(words);
+
+        assertThat(wordCount).contains(
+                MapEntry.entry("java", 3L),
+                MapEntry.entry("javascript", 1L),
+                MapEntry.entry("scala", 2L));
+
+    }
+
+    @Test
+    public void should_count_words_stream() {
+        List<String> words = new ArrayList<>();
+        words.add("java");
+        words.add("javascript");
+        words.add("java");
+        words.add("scala");
+        words.add("java");
+        words.add("scala");
+
+        Map<String, Long> wordCount = lambdaExpert.countWordStream(words);
+
+        assertThat(wordCount).contains(
+                MapEntry.entry("java", 3L),
+                MapEntry.entry("javascript", 1L),
+                MapEntry.entry("scala", 2L));
+
+    }
+
+    @Test
+    public void check_retrieveMostCommonAge() {
+        assertThat(lambdaExpert.retrieveMostCommonAge()).isEqualTo(76);
+    }
+
+    @Test
+    public void check_retrieveAverageAgeOfAllTheChildren() {
+        assertThat(lambdaExpert.firstTwoYoungerChildsByName().toString()).isEqualTo("[{\"name\":\"Adèle\",\"age\":1}, {\"name\":\"Adèle\",\"age\":1}]");
+    }
+}
