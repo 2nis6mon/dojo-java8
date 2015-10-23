48d13aee5ebdc1f6c48c76ae66612eac1761f770
Denis Simon Soneira <denis.simon@gmail.com>
2015-10-23 15:00:25 +0200

    ejercicio 4

---
+++ src/main/java/org/dojo/java8/ejercicio4/FileOperations.java
@@ -0,0 +1,94 @@
+package org.dojo.java8.ejercicio4;
+
+import org.dojo.java8.model.Address;
+import org.dojo.java8.model.Role;
+import org.dojo.java8.model.User;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class FileOperations {
+
+    //TODO: Replace By Files.lines, use static method reference
+    public static List<User> loadUsersFromCsv(Path csvPath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(csvPath.toFile()))) {
+            String line;
+            boolean firstLine = true;
+            List<User> users = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                if (!firstLine) {
+                    users.add(lineToUser(line));
+                }
+                firstLine = false;
+            }
+
+            return users;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    //TODO:replace by Files.walk and remove visitor. Use Optional.orElseThrow for throw FileNotFoundException
+    public static Path findRecursivelyFileByName(String path, String fileName) throws IOException {
+        Path rootDirectory = Paths.get(path);
+
+        SearchVisitor searchVisitor = new SearchVisitor(fileName);
+
+        Files.walkFileTree(rootDirectory, searchVisitor);
+        Path fileFound = searchVisitor.fileFound;
+        if (fileFound == null) {
+            throw new FileNotFoundException();
+        }
+        return fileFound;
+    }
+
+    public static class SearchVisitor extends SimpleFileVisitor<Path> {
+
+        private Path fileFound;
+
+        private String fileNameToSearch;
+
+        public SearchVisitor(String fileNameToSearch) {
+            this.fileNameToSearch = fileNameToSearch;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if (attrs.isRegularFile() && file.getFileName().toString().equals(fileNameToSearch)) {
+                fileFound = file;
+                return FileVisitResult.TERMINATE;
+            }
+            return FileVisitResult.CONTINUE;
+        }
+
+    }
+
+    private static User lineToUser(String line) {
+        String[] columns = line.split(",");
+        User user = new User(columns[0], columns[1], columns[2]);
+        user.withLogin(columns[3])
+                .withPassword(columns[4])
+                .withExpireDate(LocalDate.parse(columns[5], DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.")))
+                .withRole(Role.valueOf(columns[6]))
+        ;
+        if (columns.length > 8) {
+            user.withAddress(new Address(columns[7], columns[8], columns[9]));
+        }
+
+        return user;
+    }
+
+}
+
---
+++ src/test/java/org/dojo/java8/ejercicio4/FileOperationsTest.java
@@ -0,0 +1,56 @@
+package org.dojo.java8.ejercicio4;
+
+
+import org.dojo.java8.model.User;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileOperationsTest {
+
+    @Test
+    public void should_parse_user_csv() {
+        List<User> users = FileOperations.loadUsersFromCsv(getFileFromPath("users.csv"));
+
+        assertThat(users).hasSize(1000);
+        User firstUser = users.get(0);
+        assertThat(firstUser.getTitle()).isEqualTo("Mme");
+        assertThat(firstUser.getFirstname()).isEqualTo("Colette");
+        assertThat(firstUser.getLastname()).isEqualTo("Bernard");
+        assertThat(firstUser.getLogin()).isEqualTo("colettebernard");
+        assertThat(firstUser.getPassword()).isEqualTo("9876543210");
+        assertThat(firstUser.getExpireDate()).isEqualTo(LocalDate.of(2000, 12, 9));
+    }
+
+    @Test
+    public void should_find_file_path_by_filename() throws IOException {
+        Path fileWithName = FileOperations.findRecursivelyFileByName(".", "FileOperations.java");
+
+        assertThat(fileWithName).isEqualTo(Paths.get("./src/main/java/org/dojo/java8/ejercicio4/FileOperations.java"));
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void should_not_find_file_path_by_filename_when_file_not_exist() throws IOException {
+        Path fileWithName = FileOperations.findRecursivelyFileByName(".", "UserParser123.java");
+
+        assertThat(fileWithName).isNull();
+    }
+
+    private static Path getFileFromPath(String csvPath) {
+        try {
+            return Paths.get(FileOperations.class.getClassLoader().getResource(csvPath).toURI());
+        } catch (URISyntaxException e) {
+
+            return null;
+        }
+    }
+
+}
