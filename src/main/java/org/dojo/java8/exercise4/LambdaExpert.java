ee573255b2a2f749391ba7cc7058e91c1f4a8ea3
Denis Simon Soneira <denis.simon@gmail.com>
2015-11-21 11:49:27 +0100

    solution

--- src/main/java/org/dojo/java8/exercise4/LambdaExpert.java
+++ src/main/java/org/dojo/java8/exercise4/LambdaExpert.java
@@ -5,6 +5,8 @@ import org.dojo.java8.model.Person;
 import org.dojo.java8.model.PersonLibrary;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class LambdaExpert {
 
@@ -12,27 +14,13 @@ public class LambdaExpert {
 
 	// TODO use map, flatMap, sorted and limit
 	public List<Child> firstTwoYoungerChildsByName() {
-		List<Child> allChildren = new ArrayList<>();
-
-		for (Person person : persons) {
-			allChildren.addAll(person.getChildren());
-		}
-
-		Collections.sort(allChildren, new ChildComparator());
-
-		return allChildren.subList(0, 2);
-	}
-
-	private class ChildComparator implements Comparator<Child> {
-		@Override
-		public int compare(Child c1, Child c2) {
-			int nameComparaison = c1.getName().compareTo(c2.getName());
-			if (nameComparaison == 0) {
-				return Integer.compare(c1.getAge(), c2.getAge());
-			} else {
-				return nameComparaison;
-			}
-		}
+		return persons.stream()
+				.map(Person::getChildren)
+				.flatMap(Collection::stream)
+				.sorted(Comparator.comparing(Child::getName)
+						.thenComparing(Child::getAge))
+				.limit(2)
+				.collect(Collectors.toList());
 	}
 
 	// TODO NOTE! DO NOT USE STREAMS
@@ -40,70 +28,34 @@ public class LambdaExpert {
 	// TODO eventually change loop by Iterator forEach method
 	// TODO use static reference to the Integer sum method
 	public Map<String, Long> countWord(List<String> words) {
-
 		Map<String, Long> count = new HashMap<>();
-
-		for (String word : words) {
-
-			Long countOfCurrentWord = count.get(word);
-			if (countOfCurrentWord == null) {
-				countOfCurrentWord = 0L;
-			}
-
-			count.put(word, countOfCurrentWord + 1L);
-		}
-
+		words.forEach(word -> count.merge(word, 1L, Long::sum));
 		return count;
 	}
 
 	// TODO NOTE! USE STREAMS
-	// TODO use Collectors.groupingBy with Collectors.counting
-	public Map<String, Long> countWordStream(List<String> words) {
+	// TODO use Collectors.groupingBy with Collectors.counting in second argument
+	public  Map<String, Long> countWordStream(List<String> words) {
 
-		Map<String, Long> count = new HashMap<>();
-
-		for (String word : words) {
-
-			Long countOfCurrentWord = count.get(word);
-			if (countOfCurrentWord == null) {
-				countOfCurrentWord = 0L;
-			}
-
-			count.put(word, countOfCurrentWord + 1L);
-		}
-
-		return count;
+		return words.stream()
+				.collect(Collectors.groupingBy(
+						Function.identity(),
+						Collectors.counting()));
 	}
 
-	// TODO use Collectors.collectingAndThen, Collectors.groupingBy with Collectors.counting
+	// TODO use Collectors.collectingAndThen, Collectors.groupingBy with Collectors.counting in second argument
 	// TODO to complete the refactor use max map and orElse
 	public int retrieveMostCommonAge() {
-		Map<Integer, Integer> numberPeopleByAge = new HashMap<>();
-		Integer personsByAge;
-		for (Person person : persons) {
-
-			personsByAge = numberPeopleByAge.get(person.getAge());
-			if (personsByAge == null) {
-				personsByAge = 0;
-				numberPeopleByAge.put(person.getAge(), personsByAge);
-			}
-			personsByAge++;
-
-			numberPeopleByAge.put(person.getAge(), personsByAge);
-
-		}
-
-		Map.Entry<Integer, Integer> maxEntry = null;
-
-		for (Map.Entry<Integer, Integer> entry : numberPeopleByAge.entrySet()) {
-			if (maxEntry == null || entry.getValue() > maxEntry.getValue()) {
-				maxEntry = entry;
-			}
-
-		}
-
-		return maxEntry.getKey();
+		return persons.stream()
+				.collect(Collectors.collectingAndThen(
+						Collectors.groupingBy(Person::getAge, Collectors.counting()),
+						integerLongMap -> integerLongMap.entrySet()))
+				.stream()
+				.max(Comparator.comparing(Map.Entry::getValue))
+				.map(Map.Entry::getKey)
+				.orElse(0);
 
 	}
+
 
 }
