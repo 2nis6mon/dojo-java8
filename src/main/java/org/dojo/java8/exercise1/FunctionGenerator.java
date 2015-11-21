be8829d4ae0b9a0313186dd91fff5c8e7438e3a4
Denis Simon Soneira <denis.simon@gmail.com>
2015-11-21 10:52:27 +0100

    fix comments

--- src/main/java/org/dojo/java8/exercise1/FunctionGenerator.java
+++ src/main/java/org/dojo/java8/exercise1/FunctionGenerator.java
@@ -10,17 +10,17 @@ import java.util.function.*;
 public class FunctionGenerator {
 
     public static Function<Integer, Integer> plusOneFunction() {
-        //TODO: replace with lambda syntax
+        //TODO replace with lambda syntax
         return value -> value + 1;
     }
 
     public static Predicate<Integer> pairNumberPredicate() {
-        //TODO: replace with lambda syntax
+        //TODO replace with lambda syntax
         return integer -> integer % 2 == 0;
     }
 
     public static Consumer<int[]> incrementArrayValueConsumer() {
-        //TODO: replace with lambda syntax
+        //TODO replace with lambda syntax
         return arrayValues -> {
             for (int i = 0; i < arrayValues.length; i++) {
                 arrayValues[i] = arrayValues[i] + 1;
@@ -30,34 +30,34 @@ public class FunctionGenerator {
     }
 
     public static Supplier<Integer> todaySupplier() {
-        //TODO: replace with lambda syntax
+        //TODO replace with lambda syntax
         return () -> LocalDate.now().get(ChronoField.DAY_OF_MONTH);
     }
 
     public static BiFunction<String, Integer, String> concatAsStringFunction() {
-        //TODO: replace with lambda syntax
+        //TODO replace with lambda syntax
         return (firstValue, secondValue) -> firstValue + " " + secondValue;
     }
 
     // BinaryOperator is BiFunction with same generic type
     public static BinaryOperator<Integer> sumFunction() {
-        //TODO: replace with static method reference
+        //TODO replace with static method reference
         return Integer::sum;
     }
 
     public static Consumer<String> addToListConsumer(List<String> list) {
-        //TODO: replace with instance method reference
+        //TODO replace with instance method reference
         return list::add;
     }
 
     public static Function<String, BigDecimal> stringToBiDecimalFunction() {
-        //TODO: replace with constructor reference
+        //TODO replace with constructor reference
         return BigDecimal::new;
     }
 
     // Comparator is also a FunctionalInterface that can be replaced by lambda syntax
     public static Comparator<String> byStringLengthComparator() {
-        //TODO: replace with lambda syntax
+        //TODO replace with lambda syntax
         return (o1, o2) -> Integer.compare(o1.length(), o2.length());
     }
 
